7a5d4f79908598eb1f1f977990fc39e6f545623a
juanpms2 <jpablo69@hotmail.es>
2020-04-22 11:35:29 +0200

    Diseño de páginas terminado a falta de retoques.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { HomePageComponent } from "./scenes/home-page/home-page.component";
 import { MembersPageComponent } from "./scenes/members-page/members-page.component";
 import { UserPageComponent } from "./scenes/user-page/user-page.component";
 import { appRoutes } from "@app/core/routes/app.routes";
+import { NavBarComponent } from './common/components/nav-bar/nav-bar.component';
 
 @NgModule({
 	declarations: [
@@ -16,6 +17,7 @@ import { appRoutes } from "@app/core/routes/app.routes";
 		HomePageComponent,
 		MembersPageComponent,
 		UserPageComponent,
+		NavBarComponent,
 	],
 	imports: [BrowserModule, MembersModule, RouterModule.forRoot(appRoutes)],
 	providers: [],
